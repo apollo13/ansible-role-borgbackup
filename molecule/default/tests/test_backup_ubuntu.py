4b34df08750278907f62ed2fe8b16a4782407a08
Florian Apolloner <florian@apolloner.eu>
2019-06-27 13:56:09 +0200

    Configured borgbackup-tasks service for easier monitoring.

--- molecule/default/tests/test_backup_ubuntu.py
+++ molecule/default/tests/test_backup_ubuntu.py
@@ -2,6 +2,7 @@ testinfra_hosts = ["borg-ubuntu-upstream", "borg-ubuntu-system"]
 
 
 def test_backup(host):
+    # Note: is-failed returns 0 if a service failed
     assert host.run("systemctl --wait start borgbackup").rc == 0
     assert host.run("systemctl is-failed borgbackup").rc == 1
-    assert host.run("systemctl is-failed 'borgbackup@*'").rc == 0
+    assert host.run("systemctl is-failed borgbackup-tasks").rc == 0
